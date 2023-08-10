da96ffb6f7e26b84c726d8b7d92ca11267624e1a
TDMeyers <meyers.dylan@outlook.com>
2023-08-10 15:24:08 -0600

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require('express')
 
 const pokemon = require('./models/pokemon')
 
+const jsxEngine = require('jsx-view-engine')
+
 const app = express()
 
 const PORT = 3000
+
+app.set('view engine', 'jsx')
+
+app.engine('jsx', jsxEngine())
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon)
+    res.render('Index.jsx')
 })
 
 app.listen(PORT, () => {
