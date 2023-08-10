d050f1a46adbeef2e788e4ff2afe9406200be827
TDMeyers <meyers.dylan@outlook.com>
2023-08-10 15:24:30 -0600

    "index.jsx view rendered at pokemon route"

---
+++ views/Index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const myStyle = {
+    color: '#ffffff',
+    backgroundColor: '#000000',
+    };
+    
+
+export default function Index() {
+    return (
+        <div style={myStyle}>
+            <h1>
+                'See All The Pokemon!'
+            </h1>
+        </div>
+    )
+}
